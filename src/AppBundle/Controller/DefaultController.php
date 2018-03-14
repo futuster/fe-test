7e7ecf4d8e34240b180de9d9b9034b1e080b536c
futuster <futuster@gmail.com>
2018-03-14 19:09:36 +0200

    тестовое api со списком сотрудников

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
@@ -13,9 +14,43 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        return $this->render('default/index.html.twig');
+    }
+
+    /**
+     * @Route("/api/employee", name="employee_api_list")
+     */
+    public function employeeApiAction()
+    {
+        return new JsonResponse([
+            [
+                'id' => 1,
+                'name' => 'Константинопольский Константин Константинович',
+                'gender' => 'м',
+                'age' => 32,
+                'position' => 'тестировщик',
+            ],
+            [
+                'id' => 2,
+                'name' => 'Иванов Пётр',
+                'gender' => 'м',
+                'age' => 54,
+                'position' => 'системный администратор',
+            ],
+            [
+                'id' => 3,
+                'name' => 'Протопенко Светлана',
+                'gender' => 'ж',
+                'age' => 19,
+                'position' => 'офис менеджер',
+            ],
+            [
+                'id' => 5,
+                'name' => 'Энди Вачовски',
+                'gender' => '?',
+                'age' => 50,
+                'position' => 'режисер',
+            ],
         ]);
     }
 }
